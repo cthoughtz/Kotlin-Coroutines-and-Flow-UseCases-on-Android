1bf92c4f0df187bf848f2fc3af715edd039fbcc8
James Edwards <cthoughtz@gmail.com>
2023-02-12 21:49:53 -0500

    Section 3: Coroutine Fundamentals || Multithreaded Coroutines

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/fundamentals/9_coroutines_in_different_threads.kt
@@ -0,0 +1,21 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.fundamentals
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    println("main starts")
+    joinAll(
+        async { threadSwitchingCoroutine(1,500) },
+        async { threadSwitchingCoroutine(2, 300) }
+    )
+    println("main ends")
+}
+
+suspend fun threadSwitchingCoroutine(number:Int,delay: Long) {
+    println("Coroutine $number starts work on ${Thread.currentThread().name}")
+    delay(delay)
+
+    withContext(Dispatchers.IO) {
+        println("Coroutine $number has finished on ${Thread.currentThread().name}")
+    }
+}
