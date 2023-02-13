a89da3ee16dfadf47284b148b2a850a461a93735
James Edwards <cthoughtz@gmail.com>
2023-02-13 18:00:11 -0500

    Section 5: Implementing UseCase#3 in a sequential way

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,14 +1,28 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class PerformNetworkRequestsConcurrentlyViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        try {
+            uiState.value = UiState.Loading
+            viewModelScope.launch {
+                val oreoFeature = mockApi.getAndroidVersionFeatures(27)
+                val pieFeatures = mockApi.getAndroidVersionFeatures(28)
+                val android10Features = mockApi.getAndroidVersionFeatures(29)
 
+                val versionFeatures = listOf(oreoFeature,pieFeatures,android10Features)
+                uiState.value = UiState.Success(versionFeatures)
+            }
+        } catch (exception: Exception) {
+            uiState.value = UiState.Error("Network Request failed")
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
