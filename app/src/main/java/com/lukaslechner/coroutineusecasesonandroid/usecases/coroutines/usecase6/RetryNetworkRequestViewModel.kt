a13c922cf06d1b090ebdd32a8d68e53cd2f37870
James Edwards <cthoughtz@gmail.com>
2023-02-16 18:47:33 -0500

    Section 6: High-Order Function || Extract rery logic into higher order function

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,48 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+
+            val numberOfRetries = 2
+
+            try {
+               retry(numberOfRetries) {
+                   loadRecentAndroidVersions()
+            }
+                loadRecentAndroidVersions()
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
+    }
+
+    private suspend fun <T> retry(numberOfRetries: Int, block: suspend () -> T): T {
+        repeat(numberOfRetries) {
+            try {
+              return  block()
+            } catch(exception: Exception) {
+                Timber.e(exception)
+            }
+        }
+       return  block()
+    }
+
+    private suspend fun loadRecentAndroidVersions() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
