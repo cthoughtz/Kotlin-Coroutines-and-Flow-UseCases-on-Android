d07b2584e720c35ab7440ac2af488f6b17e5c333
James Edwards <cthoughtz@gmail.com>
2023-02-22 21:42:25 -0500

    Section 11: Coroutines Exception Handling || Implementation of UseCase13 with try-catch

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -1,8 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase13
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.*
+import retrofit2.HttpException
 import timber.log.Timber
 
 class ExceptionHandlingViewModel(
@@ -10,7 +12,22 @@ class ExceptionHandlingViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun handleExceptionWithTryCatch() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                api.getAndroidVersionFeatures(27)
+            } catch (exception: Exception) {
+                if(exception is HttpException) {
+                    if(exception.code() == 500) {
+                        // Error Message 1
+                    } else {
+                        // Error Message 2
+                    }
+                }
+                uiState.value = UiState.Error("Network Request failed: $exception")
+            }
+        }
     }
 
     fun handleWithCoroutineExceptionHandler() {
