9cea28a2ecd504b4d395707bbd60163e547a6577
James Edwards <cthoughtz@gmail.com>
2023-03-30 22:30:54 -0400

    Section: 15: State Flow and Shared Flow

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4Activity.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4Activity.kt
@@ -2,12 +2,18 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase4
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseActivity
 import com.lukaslechner.coroutineusecasesonandroid.base.flowUseCase4Description
 import com.lukaslechner.coroutineusecasesonandroid.databinding.ActivityFlowUsecase1Binding
 import com.lukaslechner.coroutineusecasesonandroid.utils.setGone
 import com.lukaslechner.coroutineusecasesonandroid.utils.setVisible
 import com.lukaslechner.coroutineusecasesonandroid.utils.toast
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.joda.time.LocalDateTime
 import org.joda.time.format.DateTimeFormat
 
@@ -25,11 +31,23 @@ class FlowUseCase4Activity : BaseActivity() {
         setContentView(binding.root)
         binding.recyclerView.adapter = adapter
 
-        viewModel.currentStockPriceAsLiveData.observe(this) { uiState ->
-            if (uiState != null) {
-                render(uiState)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.currentStockPriceAsFlow.collect { uiState ->
+                        render(uiState)
+                    }
+                }
+
+                launch {
+                    viewModel.currentStockPriceAsFlow.collect { uiState ->
+                        render(uiState)
+                    }
+                }
+
             }
         }
+
     }
 
     private fun render(uiState: UiState) {
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4ViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase4
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
@@ -12,7 +13,7 @@ class FlowUseCase4ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: LiveData<UiState> = stockPriceDataSource
+    val currentStockPriceAsFlow: Flow<UiState> = stockPriceDataSource
         .latestStockList
         .map { stockList ->
             UiState.Success(stockList) as UiState
@@ -23,5 +24,5 @@ class FlowUseCase4ViewModel(
         .onCompletion {
             Timber.tag("Flow").d("Flow has completed.")
         }
-        .asLiveData()
+
 }
