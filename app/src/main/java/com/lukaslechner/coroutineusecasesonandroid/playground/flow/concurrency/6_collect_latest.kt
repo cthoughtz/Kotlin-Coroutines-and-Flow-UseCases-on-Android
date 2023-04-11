760aa2ba2ac11e4190846ef087736e0945aad1e2
James Edwards <cthoughtz@gmail.com>
2023-04-11 02:19:04 -0400

    Section 17: Current Flows || collectedLatest(), MapLatest() and transformLatest()

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/concurrency/6_collect_latest.kt
@@ -0,0 +1,27 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.concurrency
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+
+suspend fun main() = coroutineScope {
+
+    val flow = flow {
+        repeat(5) {
+            println("Emitter: Start Cooking Pancake $it")
+            delay(100)
+            println("Emitter: Pancake $it ready!")
+            emit(it)
+        }
+    }
+
+    flow.collectLatest {
+        println("Collector: Start eating pancake $it")
+        delay(300)
+        println("Collector: Finished eating pancake $it")
+    }
+}
---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/concurrency/7_mapLatest.kt
@@ -0,0 +1,29 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.concurrency
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+suspend fun main() = coroutineScope {
+
+    val flow = flow {
+        repeat(5) {
+            println("Emitter: Start Cooking Pancake $it")
+            delay(100)
+            println("Emitter: Pancake $it ready!")
+            emit(it)
+        }
+    }.mapLatest {
+        println("dd topping onto pancake $it")
+        delay(200)
+        it
+    }
+
+    flow.collect {
+        println("Collector: Start eating pancake $it")
+        delay(300)
+        println("Collector: Finished eating pancake $it")
+    }
+}
